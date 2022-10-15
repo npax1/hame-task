34a525bc2dc529f471492c8add1c2b205d4204b0
npax1 <prakh55555@gmail.com>
2022-10-15 12:13:05 +0300

    add code

--- home1.py
+++ home1.py
@@ -1,9 +1,14 @@
-try:
-    a = int(input("enter num1"))
-    if a == a:
-        print("goodbay")
 
-except Exception as ex:
-    print(f"error information:{ex}")
-finally:
-    print("exit")
+start=int(input("start->"))
+end=int(input("start->"))
+counter=0
+for i in range(start< end+1):
+    counter=0
+if i==1:
+    print(i,"\t", end="")
+    continue
+for j in range (1, i+1):
+    if i % j == 0:
+        counter+=1
+if counter==2:
+    print(i,"\t",end="")
