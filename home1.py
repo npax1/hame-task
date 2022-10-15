98fe05fd057ca6d1e3808525c0a6d47a8de6c6e7
npax1 <prakh55555@gmail.com>
2022-10-15 12:13:47 +0300

    add code

--- home1.py
+++ home1.py
@@ -1,14 +1,18 @@
-
-start=int(input("start->"))
-end=int(input("start->"))
-counter=0
-for i in range(start< end+1):
+try:
+    start=int(input("start->"))
+    end=int(input("start->"))
     counter=0
-if i==1:
-    print(i,"\t", end="")
-    continue
-for j in range (1, i+1):
-    if i % j == 0:
-        counter+=1
-if counter==2:
-    print(i,"\t",end="")
+    for i in range(start< end+1):
+        counter=0
+    if i==1:
+        print(i,"\t", end="")
+        continue
+    for j in range (1, i+1):
+        if i % j == 0:
+            counter+=1
+    if counter==2:
+        print(i,"\t",end="")
+except Exception as ex:
+    print(f"error information:{ex}")
+finally:
+    print("exit")
